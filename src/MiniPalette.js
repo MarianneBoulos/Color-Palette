42a13137a8d6f8084921322a3b16535cdd131985
Marianne <marianne.boulos.mb@gmail.com>
2020-08-25 15:13:44 +0300

    Add with styles with demo styles

---
+++ src/MiniPalette.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { withStyles } from "@material-ui/styles";
+
+const styles = {
+    main: {
+        backgroundColor: "purple",
+        border: "3px solid teal"
+    },
+    secondary: {
+        backgroundColor: "pink",
+        "& h1": {
+            color: "white",
+            "& span": {
+                backgroundColor: "yellow"
+            }
+        },
+    }
+};
+
+function MiniPalette(props) {
+    const {classes} = props;
+    return(
+        <div className={classes.main}>
+         <h1>Mini Palette</h1>
+         <section className={classes.secondary}><h1>Mini Palette<span>Hi</span></h1></section>
+         <span>Hi</span>
+        </div>
+    );
+}
+
+export default withStyles(styles)(MiniPalette);
